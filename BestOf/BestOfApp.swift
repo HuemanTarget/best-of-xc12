7fda5d5d3ed47ddd2954c6571b219ba24c29bb27
HuemanTarget <joshbasche@live.com>
2021-06-15 09:03:50 -0500

    first commit

---
+++ BestOf/BestOfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestOfApp.swift
+//  BestOf
+//
+//  Created by Joshua Basche on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct BestOfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BestOf/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BestOf
+//
+//  Created by Joshua Basche on 6/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

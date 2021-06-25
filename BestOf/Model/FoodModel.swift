401ee0e25de5aa2bc8ad07d6cbb95cfeb69addff
HuemanTarget <joshbasche@live.com>
2021-06-25 17:47:35 -0500

    create Food and Attraction Model

---
+++ BestOf/Model/FoodModel.swift
@@ -0,0 +1,23 @@
+//
+//  FoodModel.swift
+//  BestOf
+//
+//  Created by Joshua Basche on 6/25/21.
+//
+
+import Foundation
+import FirebaseFirestoreSwift
+
+struct Food: Codable, Identifiable {
+  @DocumentID var id: String?
+  var location: String = ""
+  var address: String = ""
+  var votes: Int = 0
+  
+  enum CodingKeys: String, CodingKey {
+    case id
+    case location
+    case address
+    case votes
+  }
+}
